00a9525e647599b68dced42812edd26c330516ae
yubessy <yubessy0@gmail.com>
2021-02-20 15:55:37 +0900

    pass board as mutable reference inside solve

--- src/solver/vcf.rs
+++ src/solver/vcf.rs
@@ -15,12 +15,13 @@ pub fn solve(depth: u8, board: &Board, black: bool) -> Option<Vec<Point>> {
         return Some(vec![]);
     }
 
-    solve_all(depth, board, black, None)
+    let mut board = board.clone();
+    solve_all(depth, &mut board, black, None)
 }
 
 fn solve_all(
     depth: u8,
-    board: &Board,
+    board: &mut Board,
     black: bool,
     prev_move: Option<&Point>,
 ) -> Option<Vec<Point>> {
@@ -43,7 +44,8 @@ fn solve_all(
     // Continue four move
     let next_move_cands = board.sword_eyes(black);
     for next_move in &next_move_cands {
-        match solve_one(depth, board, black, next_move) {
+        let mut board = board.clone();
+        match solve_one(depth, &mut board, black, next_move) {
             Some(ps) => return Some(ps),
             None => continue,
         }
@@ -52,23 +54,23 @@ fn solve_all(
     None
 }
 
-fn solve_one(depth: u8, board: &Board, black: bool, next_move: &Point) -> Option<Vec<Point>> {
+fn solve_one(depth: u8, board: &mut Board, black: bool, next_move: &Point) -> Option<Vec<Point>> {
     if black && forbidden(board, next_move).is_some() {
         return None;
     }
-    let mut next_board = board.clone();
-    next_board.put(black, next_move);
-    let next_four_eyes = next_board.four_eyes_on(black, next_move);
+
+    board.put(black, next_move);
+    let next_four_eyes = board.four_eyes_on(black, next_move);
     if next_four_eyes.len() >= 2 {
         Some(vec![*next_move])
     } else if next_four_eyes.len() == 1 {
         let next2_move = &next_four_eyes[0];
-        if !black && forbidden(&next_board, next2_move).is_some() {
+        if !black && forbidden(&board, next2_move).is_some() {
             return Some(vec![*next_move]);
         }
-        let mut next2_board = next_board;
-        next2_board.put(!black, next2_move);
-        solve_all(depth - 1, &next2_board, black, Some(next2_move)).map(|mut ps| {
+
+        board.put(!black, next2_move);
+        solve_all(depth - 1, board, black, Some(next2_move)).map(|mut ps| {
             let mut result = vec![*next_move, *next2_move];
             result.append(&mut ps);
             result
