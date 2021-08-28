a1270587bc9f65fd134d5c921d459f3b379ecdfc
yubessy <yubessy0@gmail.com>
2021-08-28 22:12:57 +0900

    separate wasm mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,6 @@
-extern crate wasm_bindgen;
-
-use wasm_bindgen::prelude::*;
-
 pub mod bitboard;
 pub mod solver;
+pub mod wasm;
 
-use bitboard::{Board, Point};
-use std::convert::{From, TryFrom};
-
-#[wasm_bindgen]
-pub fn solve_vcf(blacks: &[u8], whites: &[u8], black: bool, depth_limit: u8) -> Option<Box<[u8]>> {
-    let mut board = Board::new();
-    let blacks = blacks.iter().map(|c| Point::try_from(c)).flatten();
-    for p in blacks {
-        board.put(true, p);
-    }
-    let whites = whites.iter().map(|c| Point::try_from(c)).flatten();
-    for p in whites {
-        board.put(false, p);
-    }
-    solver::solve(depth_limit, &mut board, black).map(|ps| {
-        ps.iter()
-            .map(|p| u8::from(p))
-            .collect::<Vec<_>>()
-            .into_boxed_slice()
-    })
-}
+pub use bitboard::{Board, Point};
+pub use wasm::solve_vcf;
