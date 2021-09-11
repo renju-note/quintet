f1f7a0870d349bc84557978dd368bb4a38ca8ded
yubessy <yubessy0@gmail.com>
2021-09-11 19:13:46 +0900

    add functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ pub mod bitboard;
 pub mod solver;
 pub mod wasm;
 
-pub use wasm::solve_vcf;
+pub use wasm::{decode_x, decode_y, encode_xy, solve_vcf};
--- src/wasm.rs
+++ src/wasm.rs
@@ -14,3 +14,38 @@ pub fn solve_vcf(blacks: &[u8], whites: &[u8], black: bool, depth_limit: u8) -> 
     let solution = solver::solve(depth_limit, &board, Player::from(black));
     solution.map(|ps| <Vec<u8>>::from(Points(ps)).into_boxed_slice())
 }
+
+#[wasm_bindgen]
+pub fn encode_xy(x: u8, y: u8) -> u8 {
+    Point(x, y).into()
+}
+
+#[wasm_bindgen]
+pub fn decode_x(code: u8) -> u8 {
+    Point::try_from(code).unwrap().0
+}
+
+#[wasm_bindgen]
+pub fn decode_y(code: u8) -> u8 {
+    Point::try_from(code).unwrap().1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encode_xy() {
+        assert_eq!(encode_xy(4, 5), 65);
+    }
+
+    #[test]
+    fn test_decode_x() {
+        assert_eq!(decode_x(65), 4);
+    }
+
+    #[test]
+    fn test_decode_y() {
+        assert_eq!(decode_y(65), 5);
+    }
+}
