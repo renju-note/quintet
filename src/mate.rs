2fec55c33fdc262dee57e7b2152478cac5fe7750
yubessy <yubessy0@gmail.com>
2022-03-10 16:37:57 +0900

    remove old vct_pn

--- src/mate.rs
+++ src/mate.rs
@@ -4,6 +4,5 @@ mod mate;
 mod vcf;
 mod vct;
 mod vct_dfpn;
-mod vct_pn;
 
-pub use mate::{solve_vcf, solve_vct, solve_vct_dfpn, solve_vct_pn};
+pub use mate::{solve_vcf, solve_vct, solve_vct_dfpn};
