3b88c5d7857099464e232dd6df4edd1035249c6e
yubessy <yubessy0@gmail.com>
2022-03-22 23:50:21 +0900

    remove useless code

--- src/mate/vct/dfpns.rs
+++ src/mate/vct/dfpns.rs
@@ -67,11 +67,6 @@ impl Searcher {
     fn expand_attack(&mut self, state: &mut State, attack: Point, threshold: Node) -> Node {
         let last2_move = state.game().last2_move();
         state.play(attack);
-        let current = self.table.lookup(state);
-        if current.pn >= threshold.pn || current.dn >= threshold.dn {
-            state.undo(last2_move);
-            return current;
-        }
         let result = self.search_defences(state, threshold);
         self.table.insert(state, result.clone());
         state.undo(last2_move);
@@ -110,11 +105,6 @@ impl Searcher {
     fn expand_defence(&mut self, state: &mut State, defence: Point, threshold: Node) -> Node {
         let last2_move = state.game().last2_move();
         state.play(defence);
-        let current = self.table.lookup(state);
-        if current.pn >= threshold.pn || current.dn >= threshold.dn {
-            state.undo(last2_move);
-            return current;
-        }
         let result = self.search_limit(state, threshold);
         self.table.insert(state, result.clone());
         state.undo(last2_move);
