05e279bdb9c8c5afccbaaf0578fc944f7a26e56d
yubessy <yubessy0@gmail.com>
2022-08-11 21:09:47 +0900

    remove look-ahead

--- src/mate/solve.rs
+++ src/mate/solve.rs
@@ -470,8 +470,6 @@ mod tests {
         let result = solve(VCTDFS, 7, &board, Black, 2);
         assert!(result.is_none());
 
-        let solution = "G12,E10,H14,H13,F12,I12,F14,G13,F13,F11,E14,D15,G14";
-
         let result = solve(VCTPNS, 7, &board, Black, 3);
         assert_eq!(path_string(result), solution);
 
@@ -514,12 +512,14 @@ mod tests {
         let result = solve(VCTDFS, 4, &board, White, 1);
         assert!(result.is_none());
 
+        let solution = "J4,K3,I4,I3,F8,G7,E6,G9,G6";
         let result = solve(VCTDFPNS, 5, &board, White, 1);
         assert_eq!(path_string(result), solution);
 
         let result = solve(VCTPNS, 5, &board, White, 1);
         assert_eq!(path_string(result), solution);
 
+        let solution = "K3,G7,I3,I4,L2,J4,L3,K2,J3";
         let result = solve(VCTLAZY, 5, &board, White, 1);
         assert_eq!(path_string(result), solution);
 
